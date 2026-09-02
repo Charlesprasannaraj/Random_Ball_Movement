24939fe5545cc6cae1a6f9d516e12ed50ee6af25
Prasannaraj <prasannaraj.t0007@gmail.com>
2026-09-02 10:57:25 +0530

    update

---
+++ script.js
@@ -0,0 +1,88 @@
+const area ={
+    element:document.getElementById('area'),
+    width: 1000,
+    height: 400,
+};
+
+// No need to change the following code
+// The initialize function creates the area div on the Html page
+
+function initialize(){
+    area.element.style.width = area.width + "px";
+    area.element.style.height = area.height + "px";
+    document.body.appendChild(area.element);
+}
+
+// No need to change the following code
+// The moveTo function moves a given ball to a set x and y coordinates
+let movement = {
+    moveTo: function(x, y){
+        this.element.style.left = x + "px";
+        this.element.style.top = y + "px";
+    },
+    // The changeDirectionIfNecessary function checks if the ball has hit the edge of the area and changes its direction if necessary
+    changeDirectionIfNecessary: function(x,y){
+        if(x < 0 || x > area.width - this.width){
+            this.dx = -this.dx;
+        }
+        if(y < 0 || y > area.height - this.height){
+            this.dy = -this.dy;
+        }
+    },
+    // TODO: implement the create function
+    create: function(color, dx,dy){
+        let newBall = Object.create(this);
+        // TODO: set the newBall.element property and initialize it to a HTML element "div"
+        // TODO: set the backgroundColor, width and height style properties for newBall.element
+        // This line set the CSS class for newBall.element. No need to change this line
+        // TODO: set the width and height of newBall based on newBall.element
+        // Hint: use the Javascript parseInt() function to convert a string value to integer
+        // TODO: use the Javascript appendChild() function to add newBall.element to the area element 
+
+        newBall.dx = dx;
+        newBall.dy = dy;
+        newBall.width= 40;
+        newBall.height= 40;
+        newBall.element = document.createElement("div");
+        newBall.element.style.backgroundColor = color;
+        newBall.element.style.width = newBall.width + "px";
+        newBall.element.style.height = newBall.height + "px";
+        newBall.element.className += "ball";
+        newBall.width = parseInt(newBall.element.style.width);
+        newBall.height = parseInt(newBall.element.style.height);
+        area.element.appendChild(newBall.element);
+        return newBall;
+        
+},
+// TODO: implement the update function
+    update: function(x,y){
+        // TODO: use the moveTo() function to move the ball
+
+        // TODO: use the javascript setTimeout() method to call changeDirectionIfNecessary() function after 100 milliseconds
+        
+        //setTimeout(changeDirectionIfNecessary(ball,x,y), 16);
+        //setTimeout(update(ball,x + ball.dx,y + ball.dy), 16);
+        // Uncomment these lines for step 1 of the activity
+        this.moveTo(x,y);
+        var ball = this;
+        setTimeout(function(){
+            ball.changeDirectionIfNecessary(x,y);
+            ball.update(x + ball.dx,y + ball.dy);
+        }, 16)
+    },
+};
+initialize();
+const ball1 = movement.create("blue", 4, 3)
+const ball2 = movement.create("red", 1, 5)
+const ball3 = movement.create("green", 2, 2)
+moveTo(ball1,1,1);
+moveTo(ball2,10,10);
+moveTo(ball3,20,20);
+
+ ball1.update(70,0);
+ ball2.update(20,200);
+ ball3.update(300,330);
+
+ if (typeof module !== 'undefined') {
+    module.exports = { update, create, changeDirectionIfNecessary, moveTo, area, movement };
+}
